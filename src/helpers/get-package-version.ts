738a7c80a3c9b33163acd766c218c4a5ce3ecac6
Rob Pocklington <robert.pocklington@xero.com>
2025-04-07 10:57:53 +1000

    Remove module requirement so it works in Claude + Mac + NVM

--- src/helpers/get-package-version.ts
+++ src/helpers/get-package-version.ts
@@ -1,14 +1,11 @@
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { readFileSync } from "fs";
+import { join } from "path";
 
 /**
  * Gets the package version from package.json
  * @returns The package version string
  */
 export function getPackageVersion(): string {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
   const packagePath = join(__dirname, '../../package.json');
   
   try {
